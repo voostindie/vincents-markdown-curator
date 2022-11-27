8abbe6e77c39d0476760b32dfaf00ac30e96e20e
Vincent Oostindië <vincent@ulso.nl>
2022-11-27 18:04:37 +0100

    Hookmarks: change in behavior.
    - Hooks are no longer cached in memory.
    - The query is only run for files that have changed in the last
      10 seconds.

--- src/main/java/nl/ulso/vmc/hook/HookmarkRepository.java
+++ src/main/java/nl/ulso/vmc/hook/HookmarkRepository.java
@@ -8,7 +8,6 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.json.JsonValue;
 import java.util.List;
-import java.util.concurrent.*;
 
 /**
  * Fetches hooks (bookmarks) for a URI from <a href="https://hookproductivity.com">Hookmark</a>.
@@ -19,43 +18,25 @@ import java.util.concurrent.*;
 public class HookmarkRepository
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(HookmarkRepository.class);
-    private static final long REFRESH_TIMEOUT = TimeUnit.MINUTES.toMillis(1);
     private static final String SCRIPT = "hookmark-bookmarks";
 
-    private final ConcurrentMap<String, Cache> cacheMap;
     private final JxaRunner jxaRunner;
 
     @Inject
     public HookmarkRepository(JxaRunner jxaRunner)
     {
-        this.cacheMap = new ConcurrentHashMap<>();
         this.jxaRunner = jxaRunner;
     }
 
     public List<Hook> listHooks(String documentUri)
     {
-        var cache = cacheMap.get(documentUri);
-        if (cache != null)
-        {
-            if (System.currentTimeMillis() - REFRESH_TIMEOUT < cache.lastUpdated())
-            {
-                LOGGER.debug("Getting hooks from cache for URI: {}", documentUri);
-                return cache.hooks();
-            }
-        }
         LOGGER.debug("Loading hooks for URI: {}", documentUri);
         var array = jxaRunner.runScriptForArray(SCRIPT, documentUri);
-        var hooks = array.stream()
+        return array.stream()
                 .map(JsonValue::asJsonObject)
                 .map(object -> new Hook(
                         object.getString("name"),
                         object.getString("address")))
                 .toList();
-        cacheMap.put(documentUri, new Cache(System.currentTimeMillis(), hooks));
-        return hooks;
-    }
-
-    record Cache(long lastUpdated, List<Hook> hooks)
-    {
     }
 }
--- src/main/java/nl/ulso/vmc/hook/HooksQuery.java
+++ src/main/java/nl/ulso/vmc/hook/HooksQuery.java
@@ -8,18 +8,22 @@ import javax.inject.Inject;
 import java.util.Map;
 
 import static java.util.Collections.emptyMap;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 /**
  * Lists all hooks (bookmarks) for a document from Hook.
  * <p/>
- * If the current document is archived, then the query doesn't run and instead returns a no-op.
- * Any existing output will remain. This is to ensure that old, archived notes don't end up
- * with an empty list of hooks; the list of hooks is essentially frozen on archival.
+ * If the current document hasn't been saved to disk recently, then the query doesn't run and
+ * instead returns a no-op. Any existing output will remain. This is done because the number
+ * of hook queries typically only grows, but only very few (the recent ones) have changing output.
+ * <p/>
+ * If the hooks in a document need to be updated even though the document doesn't require changes,
+ * a simple `touch` is enough.
  */
 public class HooksQuery
         implements Query
 {
-    private static final String ARCHIVE_PATH = "/_Archive/";
+    private static final long MODIFICATION_THRESHOLD_MILLISECONDS = SECONDS.toMillis(10);
     private final HookmarkRepository repository;
     private final DocumentPathResolver documentPathResolver;
     private final QueryResultFactory resultFactory;
@@ -43,7 +47,8 @@ public class HooksQuery
     @Override
     public String description()
     {
-        return "Lists all hooks for the current document";
+        return "Lists all hooks for the current document. This query runs only if the document " +
+               "has been saved to disk in the last 10 seconds!";
     }
 
     @Override
@@ -55,22 +60,23 @@ public class HooksQuery
     @Override
     public QueryResult run(QueryDefinition definition)
     {
-        var documentUri = resolveUri(definition.document());
-        if (isDocumentArchived(documentUri))
+        if (isQueryDisabled(definition))
         {
             return resultFactory.noOp();
         }
+        var documentUri = resolveUri(definition.document());
         return resultFactory.unorderedList(
                 repository.listHooks(documentUri).stream().map(Hook::toMarkdown).toList());
+    }
+
+    private static boolean isQueryDisabled(QueryDefinition definition)
+    {
+        return definition.document().lastModified() + MODIFICATION_THRESHOLD_MILLISECONDS <
+               System.currentTimeMillis();
     }
 
     private String resolveUri(Document document)
     {
         return documentPathResolver.resolveAbsolutePath(document).toUri().toString();
-    }
-
-    private boolean isDocumentArchived(String documentUri)
-    {
-        return documentUri.contains(ARCHIVE_PATH);
     }
 }
