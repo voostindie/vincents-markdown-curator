58f6453f99258833dea1298646cdf76178505a24
Vincent Oostindië <vincent@ulso.nl>
2023-03-19 10:39:29 +0100

    Remove unused code.

--- src/main/java/nl/ulso/vmc/rabobank/JournalEntry.java
+++
@@ -1,9 +0,0 @@
-package nl.ulso.vmc.rabobank;
-
-import nl.ulso.markdown_curator.vault.Section;
-
-import java.time.LocalDate;
-
-record JournalEntry(LocalDate date, String folder, Section section, String subject)
-{
-}
