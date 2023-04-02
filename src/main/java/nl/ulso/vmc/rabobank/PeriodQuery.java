7395159538957991d9c9bffd5dae40665677f760
Vincent Oostindië <vincent@ulso.nl>
2023-04-02 12:39:14 +0200

    Generalize WeekQuery into PeriodQuery.
    The PeriodQuery generates reports for any given period, given a start
    date and a number of days.
    
    The WeekQuery specializes it by selecting all days in a certain week.

---
+++ src/main/java/nl/ulso/vmc/rabobank/PeriodQuery.java
@@ -0,0 +1,114 @@
+package nl.ulso.vmc.rabobank;
+
+import nl.ulso.markdown_curator.journal.Journal;
+import nl.ulso.markdown_curator.query.*;
+import nl.ulso.markdown_curator.vault.*;
+
+import javax.inject.Inject;
+import java.time.LocalDate;
+import java.util.*;
+
+import static java.util.Comparator.comparing;
+
+public class PeriodQuery
+        implements Query
+{
+    static final String DEFAULT_FOLDER = "Projects";
+    private final Journal model;
+    private final QueryResultFactory resultFactory;
+
+
+    @Inject
+    PeriodQuery(Journal model, QueryResultFactory resultFactory)
+    {
+        this.model = model;
+        this.resultFactory = resultFactory;
+    }
+
+    @Override
+    public String name()
+    {
+        return "period";
+    }
+
+    @Override
+    public String description()
+    {
+        return "Generates an overview of notes touched in a certain period, " +
+               "extracted from the journal";
+    }
+
+    @Override
+    public Map<String, String> supportedConfiguration()
+    {
+        return Map.of(
+                "start", "First date to include in the period, in YYYY-MM-DD format",
+                "duration", "Number of days in the period, defaults to 7",
+                "folder", "Folder of notes to report on; defaults to '" + DEFAULT_FOLDER + "'"
+        );
+    }
+
+    @Override
+    public final QueryResult run(QueryDefinition definition)
+    {
+        LocalDate start = resolveStartDate(definition);
+        if (start == null)
+        {
+            return resultFactory.error("No valid start date specified");
+        }
+        var duration = resolveDuration(definition);
+        if (duration < 0)
+        {
+            return resultFactory.error("Invalid duration specified");
+        }
+        var documentNames = model.referencedDocumentsIn(start, duration);
+        var folder = definition.configuration().string("folder", DEFAULT_FOLDER);
+        var finder = new DocumentFinder(documentNames, folder);
+        model.vault().accept(finder);
+        return resultFactory.unorderedList(
+                finder.selectedDocuments.stream()
+                        .sorted(comparing(Document::sortableTitle))
+                        .map(Document::link)
+                        .toList());
+    }
+
+    protected LocalDate resolveStartDate(QueryDefinition definition)
+    {
+        return definition.configuration().date("start", null);
+    }
+
+    protected int resolveDuration(QueryDefinition definition)
+    {
+        return definition.configuration().integer("duration", 7);
+    }
+
+    private static class DocumentFinder
+            extends BreadthFirstVaultVisitor
+    {
+        private final Set<String> documentNames;
+        private final String selectedFolderName;
+        private final Set<Document> selectedDocuments;
+
+        DocumentFinder(Set<String> documentNames, String folderName)
+        {
+            this.documentNames = documentNames;
+            this.selectedFolderName = folderName;
+            selectedDocuments = new HashSet<>();
+        }
+
+        @Override
+        public void visit(Vault vault)
+        {
+            vault.folder(selectedFolderName).ifPresent(folder -> folder.accept(this));
+        }
+
+        @Override
+        public void visit(Document document)
+        {
+            if (documentNames.contains(document.name()))
+            {
+                selectedDocuments.add(document);
+            }
+        }
+    }
+}
--- src/main/java/nl/ulso/vmc/rabobank/WeeklyQuery.java
+++ src/main/java/nl/ulso/vmc/rabobank/WeeklyQuery.java
@@ -1,35 +1,33 @@
 package nl.ulso.vmc.rabobank;
 
 import nl.ulso.markdown_curator.journal.Journal;
-import nl.ulso.markdown_curator.query.*;
-import nl.ulso.markdown_curator.vault.*;
+import nl.ulso.markdown_curator.query.QueryDefinition;
+import nl.ulso.markdown_curator.query.QueryResultFactory;
 
 import javax.inject.Inject;
 import java.time.DateTimeException;
 import java.time.LocalDate;
-import java.util.*;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 import static java.lang.Integer.parseInt;
 import static java.time.LocalDate.now;
 import static java.time.temporal.ChronoField.DAY_OF_WEEK;
 import static java.time.temporal.IsoFields.WEEK_OF_WEEK_BASED_YEAR;
-import static java.util.Comparator.comparing;
 import static java.util.regex.Pattern.compile;
 
 public class WeeklyQuery
-        implements Query
+        extends PeriodQuery
 {
-    private static final Pattern WEEKLY_DOCUMENT_NAME_PATTERN = compile("^(\\d{4}) Week (\\d{2})");
-    private static final String DEFAULT_FOLDER = "Projects";
-    private final Journal model;
-    private final QueryResultFactory resultFactory;
+    private static final Pattern WEEKLY_DOCUMENT_NAME_PATTERN = compile("^(\\d{4}) Week (\\d{2})$");
+    private static final int FIRST_DAY_OF_THE_WEEK = 1;
+    private static final int INVALID = -1;
+    private static final int ONE_WEEK = 7;
 
     @Inject
     WeeklyQuery(Journal model, QueryResultFactory resultFactory)
     {
-        this.model = model;
-        this.resultFactory = resultFactory;
+        super(model, resultFactory);
     }
 
     @Override
@@ -55,71 +53,39 @@ public class WeeklyQuery
     }
 
     @Override
-    public QueryResult run(QueryDefinition definition)
+    protected LocalDate resolveStartDate(QueryDefinition definition)
     {
-        var year = definition.configuration().integer("year", -1);
-        var week = definition.configuration().integer("week", -1);
-        var folder = definition.configuration().string("folder", DEFAULT_FOLDER);
-        if (year == -1 || week == -1)
+        var year = definition.configuration().integer("year", INVALID);
+        var week = definition.configuration().integer("week", INVALID);
+        if (year == INVALID || week == INVALID)
         {
             var matcher = WEEKLY_DOCUMENT_NAME_PATTERN.matcher(definition.document().name());
             if (matcher.matches())
             {
-                year = year != -1 ? year : parseInt(matcher.group(1));
-                week = week != -1 ? week : parseInt(matcher.group(2));
+                year = year != INVALID ? year : parseInt(matcher.group(1));
+                week = week != INVALID ? week : parseInt(matcher.group(2));
             }
             else
             {
-                return resultFactory.error("Invalid year and/or week specified");
+                return null;
             }
         }
-        LocalDate start;
         try
         {
-            start = now().withYear(year).with(WEEK_OF_WEEK_BASED_YEAR, week).with(DAY_OF_WEEK, 1);
+            return now()
+                    .withYear(year)
+                    .with(WEEK_OF_WEEK_BASED_YEAR, week)
+                    .with(DAY_OF_WEEK, FIRST_DAY_OF_THE_WEEK);
         }
         catch (DateTimeException e)
         {
-            return resultFactory.error(
-                    "Invalid date for year " + year + " and week " + week + ":  " + e.getMessage());
+            return null;
         }
-        var documentNames = model.referencedDocumentsIn(start, 7);
-        var finder = new DocumentFinder(documentNames, folder);
-        model.vault().accept(finder);
-        return resultFactory.unorderedList(
-                finder.selectedDocuments.stream()
-                        .sorted(comparing(Document::sortableTitle))
-                        .map(Document::link)
-                        .toList());
     }
 
-    private static class DocumentFinder
-            extends BreadthFirstVaultVisitor
+    @Override
+    protected int resolveDuration(QueryDefinition definition)
     {
-        private final Set<String> documentNames;
-        private final String selectedFolderName;
-        private final Set<Document> selectedDocuments;
-
-        DocumentFinder(Set<String> documentNames, String folderName)
-        {
-            this.documentNames = documentNames;
-            this.selectedFolderName = folderName;
-            selectedDocuments = new HashSet<>();
-        }
-
-        @Override
-        public void visit(Vault vault)
-        {
-            vault.folder(selectedFolderName).ifPresent(folder -> folder.accept(this));
-        }
-
-        @Override
-        public void visit(Document document)
-        {
-            if (documentNames.contains(document.name()))
-            {
-                selectedDocuments.add(document);
-            }
-        }
+        return ONE_WEEK;
     }
 }
