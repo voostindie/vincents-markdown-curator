506dd7360298976bb8a080fa878dba363fbee06b
Vincent Oostindië <vincent@ulso.nl>
2026-05-23 22:13:10 +0200

    DemoCuratorModule: update to align with the markdown-curator-demo.

--- src/main/java/nl/ulso/vmc/demo/DemoCuratorModule.java
+++ src/main/java/nl/ulso/vmc/demo/DemoCuratorModule.java
@@ -2,15 +2,18 @@ package nl.ulso.vmc.demo;
 
 import dagger.Module;
 import dagger.Provides;
+import jakarta.inject.Named;
 import nl.ulso.curator.CuratorModule;
-import nl.ulso.curator.addon.journal.JournalModule;
 import nl.ulso.curator.addon.journal.JournalSettings;
+import nl.ulso.curator.addon.project.ProjectSettings;
+import nl.ulso.curator.addon.projectjournal.ProjectJournalModule;
 
 import java.nio.file.Path;
 
+import static nl.ulso.curator.CuratorModule.WATCH_DOCUMENT_KEY;
 import static nl.ulso.curator.VaultPaths.pathInUserHome;
 
-@Module(includes = {CuratorModule.class, JournalModule.class})
+@Module(includes = {CuratorModule.class, ProjectJournalModule.class})
 public class DemoCuratorModule
 {
     @Provides
@@ -20,13 +23,27 @@ public class DemoCuratorModule
     }
 
     @Provides
+    @Named(WATCH_DOCUMENT_KEY)
+    static String watchDocument()
+    {
+        return "WATCHDOC";
+    }
+
+
+    @Provides
+    ProjectSettings projectSettings()
+    {
+        return new ProjectSettings("Projects");
+    }
+
+    @Provides
     JournalSettings journalSettings()
     {
         return new JournalSettings(
-                "Journal",
-                "Markers",
-                "Activities",
-                "Projects"
+            "Journal",
+            "Markers",
+            "Activities",
+            "Projects"
         );
     }
 }
