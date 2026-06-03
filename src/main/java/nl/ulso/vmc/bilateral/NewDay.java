c7db9c402816494db46c2de0cff25214aef7ba4f
Vincent Oostindië <vincent@ulso.nl>
2026-06-03 19:32:43 +0200

    Add toString to NewDay.

--- src/main/java/nl/ulso/vmc/bilateral/NewDay.java
+++ src/main/java/nl/ulso/vmc/bilateral/NewDay.java
@@ -2,9 +2,17 @@ package nl.ulso.vmc.bilateral;
 
 import nl.ulso.curator.change.Change;
 
+import static nl.ulso.curator.change.Change.create;
+
 /// Event that is triggered once a day, as close to midnight as possible, when the machine is
 /// active.
 record NewDay()
 {
-    static Change<NewDay> NEW_DAY = Change.create(new NewDay(), NewDay.class);
+    static Change<NewDay> NEW_DAY = create(new NewDay(), NewDay.class);
+
+    @Override
+    public String toString()
+    {
+        return ".";
+    }
 }
